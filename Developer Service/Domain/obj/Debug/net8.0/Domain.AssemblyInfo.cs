245ccaa5ddc48461918a0cfce6318ea9eb892d73
Sabith-asp <abdullasabithpulli@gmail.com>
2025-06-17 17:19:32 +0530

    resolving solution issue

--- Developer Service/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Developer Service/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685dfdeaf1d072762147daaf0accec6f52483b52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff1e35d13728a2173d19180dec4b2eccea384d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
