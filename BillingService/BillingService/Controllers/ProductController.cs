b72c94561da284b2fc7ad2c31024af9d3298ba23
Sabith-asp <abdullasabithpulli@gmail.com>
2025-06-17 19:31:23 +0530

    product consuming grpc completed

---
+++ BillingService/BillingService/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using BillingService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductConsumerService _productService;
+
+        public ProductController(ProductConsumerService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{orgId}")]
+        public async Task<IActionResult> GetProducts(Guid orgId)
+        {
+            var products = await _productService.GetProductsByOrganizationIdAsync(orgId);
+
+            if (products.Count == 0)
+            {
+                return NotFound("No products found.");
+            }
+
+            return Ok(products);
+        }
+    }
+}
--- BillingService/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ BillingService/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff1e35d13728a2173d19180dec4b2eccea384d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cc34c48fdb12d74d0809c4a2a854f6de4ee590")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
