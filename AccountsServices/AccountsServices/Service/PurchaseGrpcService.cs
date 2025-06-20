aba2c1aead234d487510f62f3a50f9d855598bef
ramees27 <rameeskt27@gmail.com>
2025-06-20 14:40:57 +0530

    Added purchase grpc

---
+++ AccountsServices/AccountsServices/Service/PurchaseGrpcService.cs
@@ -0,0 +1,55 @@
+﻿using Application.DTO;
+using Application.Interfaces.IServices;
+using Application.Services.AccountsService;
+//using GroupGrpc;
+using Grpc.Core;
+using Org.BouncyCastle.Asn1.Ocsp;
+using PurchaseGrpc;
+
+namespace AccountsServices.Service
+{
+    public class PurchaseGrpcService : VoucherGrpcService.VoucherGrpcServiceBase
+    {
+        private readonly IVoucherService _voucherService;
+        public PurchaseGrpcService(IVoucherService voucherService)
+        {
+            _voucherService = voucherService;   
+        }
+        public override async Task<ApiResponses> AddVoucherEntry(AddVoucherRequest addVoucherRequest,ServerCallContext context)
+        {
+            var addVoucherDTO= new AddVouchersDTO
+            {
+                VoucherTypeId=Guid.Parse(addVoucherRequest.VoucherTypeId),
+                VoucherDate= DateTime.Parse(addVoucherRequest.VoucherDate),
+                Remarks=addVoucherRequest.Remarks,
+                TransactionsDebit=addVoucherRequest.TransactionsDebit.Select(x=>new TransactionsDTO
+                {
+                    LedgerId=Guid.Parse(x.LedgerId),
+                    Amount= Convert.ToDecimal(x.Amount),
+                   
+                    Narration=x.Narration,
+                    VoucherId= Guid.Parse(x.VoucherId),
+                    
+                    
+                }).ToList(),
+                TransactionsCredit = addVoucherRequest.TransactionsCredit.Select(x => new TransactionsDTO
+                {
+                    LedgerId = Guid.Parse(x.LedgerId),
+                    Amount = Convert.ToDecimal(x.Amount),
+                    Narration = x.Narration,
+                 
+                }).ToList()
+            };
+
+            var response = await _voucherService.AddVoucherEntrys(Guid.Parse(addVoucherRequest.OrganizationId), Guid.Parse(addVoucherRequest.CreatedBy), addVoucherDTO);
+
+            return new ApiResponses
+            {
+                StatusCode = response.StatusCode,
+                Message = response.Message,
+                Data = response.Data
+            };
+        }
+        
+    }
+}
